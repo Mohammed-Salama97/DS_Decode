2e7c659e40c4bb2285dc054b5e5d5085df900e00
mo_salama <mohamedsalama@coral.io>
2023-11-08 14:36:57 +0200

    start the project

---
+++ src/Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args) {
+
+        int [] students = new int[2];
+        String name [] = new String[2];
+        students[0] = 4;
+        name[1] = "Sa3d";
+
+        
+        System.out.println(name[0]);
+        System.out.println(students[1]);
+        System.out.println(students[0]);
+
+    }
+
+}
